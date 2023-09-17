250f4ad43a3e789595f648e634de9e832c8fc9d1
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 23:01:00 -0500

    Add files via upload

---
+++ 04.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double soles = 100.0;
+        double tc = 0.1116;
+        double euros = soles * tc;
+        System.out.println("Se tendra: " + euros + " euros.");
+    }
+}
